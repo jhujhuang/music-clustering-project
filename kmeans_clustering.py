7de65531ed2621d71e064a575ae8e5af68d1af92
Jiawei Huang <jhujhuang@gmail.com>
2015-11-19 13:49:29 -0500

    tidy up

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -5,12 +5,20 @@ from os.path import isfile, join
 
 _FEATURE_DIR = './features/'
 
+
 def read(filename):
-    csv_files = [f for f in listdir(_FEATURE_DIR)
-                 if isfile(join(_FEATURE_DIR, f))
-                 and filename + '.' in f]
-    for csv_name in csv_files:
-        with open(csv_name, 'rb') as csvfile:
-            csv_reader = csv.reader(csvfile)
+    """
+    Read all csv files for features associated with given filename.
+
+    :param filename: String, name of file including '.mp3'
+    :return: (object with) tuple representing features
+    """
+    files = [f for f in listdir(_FEATURE_DIR) if isfile(join(_FEATURE_DIR, f))
+             and filename + '.' in f]
+    for csv_name in files:
+        with open(csv_name, 'rb') as csv_file:
+            csv_reader = csv.reader(csv_file)
             for row in csv_reader:
                 pass
+
+read('mozartG.mp3')

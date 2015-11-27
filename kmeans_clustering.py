35c7fbe9753bac523f3e870f672ee2b64673aea3
Jiawei Huang <jhujhuang@gmail.com>
2015-11-27 01:05:17 -0500

    use external clustering library

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -4,6 +4,7 @@ import numpy
 from os import listdir
 from os.path import isfile, join
 from shutil import copy
+from kmeans_lib import find_centers
 
 _FEATURE_DIR = './features/'
 _MUSIC_DIR = './musicFiles'
@@ -20,7 +21,7 @@ def read(filename):
     """
     files = [f for f in listdir(_FEATURE_DIR) if isfile(join(_FEATURE_DIR, f))
              and filename + '.' in f]
-    t = ()
+    vector = []
     print filename
     for csv_name in files:
         with open(join(_FEATURE_DIR, csv_name), 'rb') as csv_file:
@@ -30,21 +31,35 @@ def read(filename):
                 if hi:
                     print row
                 hi = False
-            t = t + (csv_reader.line_num,)"""  # using numpy now
+            t = t + (csv_reader.line_num,)"""  # using numpy & list now
             csv_data = numpy.genfromtxt(csv_file, comments='%', delimiter=',')
-            t = t + (numpy.average(csv_data), numpy.var(csv_data))
-    return t  # TODO: return a feature tuple thing
+            vector.append(numpy.average(csv_data))
+            vector.append(numpy.var(csv_data))
+    return vector  # TODO: return a feature tuple thing
+
+samples = []  # For storing which features is from which file
 
 # Get features for all input samples
 musicFiles = [f for f in listdir(_MUSIC_DIR) if '.mp3' in f]
 allInput = []
 for musicFile in musicFiles:
-    features = read(musicFile)
+    features = numpy.array(read(musicFile))
     allInput.append(features)
-
+    samples.append((features, musicFile,))
 print allInput  # TODO: delete
 
 # Clustering
+_,clusters = find_centers(allInput, 4)
+
+def get_filename(feature_vector):
+    for (fv, name) in samples:
+        if numpy.array_equal(fv,feature_vector):
+            return name
+
+for key in clusters:
+    print "\n" + str(key) + ":\n"
+    for fv in clusters[key]:
+        print get_filename(fv) + " "
 
 # TODO: output files to clustered directories
 # for f in musicFiles:

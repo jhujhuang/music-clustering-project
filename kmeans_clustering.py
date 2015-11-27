ca16ff92cb9ab9ffead9659230d8620ab94b0095
Jiawei Huang <jhujhuang@gmail.com>
2015-11-26 23:06:11 -0500

    get features for mean and variances from each csv file

--- kmeans_clustering.py
+++ kmeans_clustering.py
@@ -1,11 +1,14 @@
 
 import csv
+import numpy
 from os import listdir
 from os.path import isfile, join
 from shutil import copy
 
 _FEATURE_DIR = './features/'
 _MUSIC_DIR = './musicFiles'
+
+K = 4  # Number of clusters
 
 
 def read(filename):
@@ -17,13 +20,22 @@ def read(filename):
     """
     files = [f for f in listdir(_FEATURE_DIR) if isfile(join(_FEATURE_DIR, f))
              and filename + '.' in f]
+    t = ()
+    print filename
     for csv_name in files:
         with open(join(_FEATURE_DIR, csv_name), 'rb') as csv_file:
-            csv_reader = csv.reader(csv_file)
+            """csv_reader = csv.reader(csv_file)
+            hi = True
             for row in csv_reader:
-                pass
-    return ()  # TODO: return a feature tuple thing
+                if hi:
+                    print row
+                hi = False
+            t = t + (csv_reader.line_num,)"""  # using numpy now
+            csv_data = numpy.genfromtxt(csv_file, comments='%', delimiter=',')
+            t = t + (numpy.average(csv_data), numpy.var(csv_data))
+    return t  # TODO: return a feature tuple thing
 
+# Get features for all input samples
 musicFiles = [f for f in listdir(_MUSIC_DIR) if '.mp3' in f]
 allInput = []
 for musicFile in musicFiles:
@@ -31,6 +43,8 @@ for musicFile in musicFiles:
     allInput.append(features)
 
 print allInput  # TODO: delete
+
+# Clustering
 
 # TODO: output files to clustered directories
 # for f in musicFiles:

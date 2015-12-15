d132ac1f8c77d3d2782dbb1cddb307f0178a265f
Jiawei Huang <jhujhuang@gmail.com>
2015-12-15 15:22:39 -0500

    initial gmm_lib

--- gmm_lib.py
+++ gmm_lib.py
@@ -1,2 +1,25 @@
 import random
 import numpy
+
+class GMM(object):
+
+    def __init__(self, data, k):
+        self.data = data
+        self.k = k
+        self.initialize()
+
+    def initialize(self):
+        self.mu
+        self.cov
+        self.pi
+        # TODO
+
+def cluster(data, k):
+    """
+    Perform GMM clustering on given data with given number of clusters.
+
+    :param data: List of array data points of same dimensions
+    :param k: Number of clusters
+    :return:
+    """
+    pass

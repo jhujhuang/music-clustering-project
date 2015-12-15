b161c162316e284e7e30569dabc2eebfa98df71d
Jiawei Huang <jhujhuang@gmail.com>
2015-12-15 16:53:27 -0500

    basic skeleton of gmm

--- gmm_lib.py
+++ gmm_lib.py
@@ -1,20 +1,37 @@
 import random
 import numpy
+import kmeans_lib
+
 
 class GMM(object):
 
-    def __init__(self, data, k):
+    def __init__(self, data, k, plusplus=False):
         self.data = data
         self.k = k
-        self.initialize()
+        # Initialize centers with/without kmeans++ option
+        self.mu = self._initialize_centers(plusplus)
+        # Initialize covariance as identity matrices
+        m = len(data[0])
+        self.cov = numpy.identity(m) * k
+        # Initialize pi as equally likely
+        self.pi = numpy.ones(k) / k
 
-    def initialize(self):
-        self.mu
-        self.cov
-        self.pi
-        # TODO
+    def _initialize_centers(self, pp):
+        if pp:
+            return kmeans_lib.get_kmeans_plusplus(self.data, self.k)
+        else:
+            return random.sample(self.data, self.k)
 
-def cluster(data, k):
+    def find_clusters(self):
+        # Run EM until convergence
+        while not self._has_converged():
+            pass  # TODO
+
+    def _has_converged(self):
+        pass
+
+
+def cluster(data, k, plusplus=False):
     """
     Perform GMM clustering on given data with given number of clusters.
 
@@ -22,4 +39,5 @@ def cluster(data, k):
     :param k: Number of clusters
     :return:
     """
-    pass
+    gmm = GMM(data, k, plusplus)
+    return gmm.find_clusters()
